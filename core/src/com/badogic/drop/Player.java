d724f522985bf6af0b4871ace1cfc466e6ec9344
alex <alex@far-home>
2017-11-18 00:02:55 +0100

    added might

--- core/src/com/badogic/drop/Player.java
+++ core/src/com/badogic/drop/Player.java
@@ -6,6 +6,15 @@ public class Player {
 	public String name; 
 	public String gender; 
 	public String race;
+	public double might; 
 	
+	public Player(String name, String gender, String race) {
+		this.geld = 100;
+		this.name = name;
+		this.gender = gender;
+		this.race = race;
+		this.might = 1.0;
+					
+	}
 
 }
