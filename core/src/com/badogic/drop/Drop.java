eecc83c4b3abfba207e2e39d7f6c9b442940283d
alex <alex@far-home>
2017-11-17 22:12:49 +0100

    Erster Gamestate / GUI / Screen Kram

--- core/src/com/badogic/drop/Drop.java
+++ core/src/com/badogic/drop/Drop.java
@@ -1,11 +1,9 @@
 package com.badogic.drop;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badogic.drop.Game;
+
 
 
 /*Gamestates:
@@ -19,37 +17,28 @@ import com.badogic.drop.Game;
  * 
  */
 
-
-public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Game game;
-	int gamestate;
+public class Drop extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
+	
 	
 	
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		game = new Game(400.0,5000,0,0);
-		//Testkommentar f�r Git
-		//Test 2 von Linux 
-		//Test Christina2
-		//Test Kevin
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 	
 	@Override
-	public void dispose () {
+	public void render() {
+		super.render(); //important!
+	}
+	
+	@Override
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
