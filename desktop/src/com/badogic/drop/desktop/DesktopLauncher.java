191ca07fd5ab54b5d0a74992c5c149d4b1cb5362
Danduriel <meckzankzwerg@yahoo.de>
2017-11-17 11:36:48 +0100

    Initial upload

---
+++ desktop/src/com/badogic/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badogic.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badogic.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
