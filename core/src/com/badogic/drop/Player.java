15cbab91698fc1e4b4e79b23b165fbfc23b7a12e
Xenex <kevin@hasenbach.org>
2017-11-18 03:26:22 +0100

    Added beer, dynamite in Player

--- core/src/com/badogic/drop/Player.java
+++ core/src/com/badogic/drop/Player.java
@@ -2,6 +2,8 @@ package com.badogic.drop;
 
 public class Player {
 	
+	public int dynamite;
+	public int beer;
 	public int money; 
 	public String name; 
 	public String gender; 
@@ -9,6 +11,8 @@ public class Player {
 	public double might; 
 	
 	public Player(String name, String gender, String race) {
+		this.beer = 0;
+		this.dynamite = 0;
 		this.money = 100;
 		this.name = name;
 		this.gender = gender;
