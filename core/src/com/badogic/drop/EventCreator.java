5e75233921e046b88050b9942d6f84c22c75d7c8
harald <harald@Harals-Doenerbude>
2017-11-18 20:55:50 +0100

    Ich hab dich gefuckupt.

--- core/src/com/badogic/drop/EventCreator.java
+++ core/src/com/badogic/drop/EventCreator.java
@@ -13,12 +13,7 @@ public class EventCreator {
 		neutralEvents.add(new MyEvent("test neutral", // Event Name
 									"test description neutral", // Event description
 									0,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
-		neutralEvents.add(new MyEvent("test neutral", // Event Name
-				"test description neutral", // Event description
-				0,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
 
-		
-		
 		
 		//Add good Events
 		goodEvents.add(new MyEvent("test good", // Event Name
@@ -26,7 +21,41 @@ public class EventCreator {
 									0,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
 		
 		
+		//Add good Events
+		goodEvents.add(new MyEvent("Dynamit LKW hat Last verloren", // Event Name
+									"Ein vorbeifahrender LKW hat Dynamit verloren, ja im Jungel fahren LKWs.", // Event description
+									+4,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
 		
+		//Add good Events
+		goodEvents.add(new MyEvent("Zeppelinabsturz", // Event Name
+									"Die Hindenburg 3.1 ist nahe deines Lagers abgestürzt und hat Güter verloren.", // Event description
+									0,+5,+6,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+		
+		//Add good Events
+		goodEvents.add(new MyEvent("Flussschwemme", // Event Name
+											"Der naheliegende Fluss ist überschwemmt und hat Bierfässer mitgetragen.", // Event description
+											0,+4,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+				
+		//Add good Events
+		goodEvents.add(new MyEvent("Local Outlet Sale", // Event Name
+											"Das Sprengstoff Outlet deiner Wahl hatte einen Sale", // Event description
+											+5,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+				
+
+		//Add good Events
+		goodEvents.add(new MyEvent("B-Ware", // Event Name
+									"Dein Sprengstoff-Guy schenkt dir als Treuebonus etwas B-Ware", // Event description
+									+2,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+
+		//Add good Events
+		goodEvents.add(new MyEvent("Gnädige Delphinsuperintelligenz", // Event Name
+									"Die eigentliche Superintelligenz auf der Erde, aka. Delphine, unterstützen dich.", // Event description
+									+5,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+
+		//Add good Events
+		goodEvents.add(new MyEvent("Geheime Elfentechonologie", // Event Name
+									"Ein Zug, ja ein Zug im Jungel, wirft dir Sprengstoff ab. Choo Choo.", // Event description
+									+4,0,0,0,0,0,0)); 		// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
 		//Add bad Events
 		badEvents.add(new MyEvent("test bad", // Event Name
 								"test description bad", // Event description
